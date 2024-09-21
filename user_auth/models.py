de6bd6f3879e2b56e966d29591b3d201effa50f5
Sudipto Kumar Shil <sudiptoshil@outlook.com>
2024-09-21 18:08:10 +0600

    User auth admin added

--- user_auth/models.py
+++ user_auth/models.py
@@ -5,3 +5,6 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
     image = models.ImageField(upload_to="users/")
+
+    def __str__(self) -> str:
+        return self.user.username
